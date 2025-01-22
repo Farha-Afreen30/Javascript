17acbebd9a88c09bfb02112fbf5f0a7d4f687515
Farha-Afreen30 <Farha.Afreen@rootquotient.com>
2025-01-22 12:09:03 +0530

    Function currying

--- currying.js
+++ currying.js
@@ -1,3 +1,5 @@
+//using bind
+
 let multiply = function (x,y) {
   console.log(x*y)
 }
@@ -6,4 +8,18 @@ let multiplybyTwo = multiply.bind(this,2);
 multiplybyTwo(10);
 
 let multiplybyThree = multiply.bind(this,3);
+multiplybyThree(10);
+
+
+//using closures
+let multiply_closure = function (x) {
+  return function(y) {
+    console.log(x*y);
+  }
+}
+
+let multiplyByTwo = multiply(2);
+multiplybyTwo(10);
+
+let multiplyByThree = multiply(3);
 multiplybyThree(10);
