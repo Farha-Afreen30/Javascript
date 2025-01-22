2f228e2bf166962cd0dcf65531bbf09c06cbced0
Farha-Afreen30 <Farha.Afreen@rootquotient.com>
2025-01-22 13:33:05 +0530

    Debouncing Throttling example

---
+++ debouncing_throttling.js
@@ -0,0 +1,54 @@
+//Debouncing
+function debounce(func, delay) {
+  let timer;
+  return function(...args) {
+    clearTimeout(timer); // Clear the previous timer
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
+
+function logMessage(message) {
+  console.log("Debounced: " + message);
+}
+
+const debouncedFunction = debounce(logMessage, 300);
+
+debouncedFunction("Call 1");
+debouncedFunction("Call 2");
+debouncedFunction("Call 3");
+debouncedFunction("Call 4");
+debouncedFunction("Call 5");
+debouncedFunction("Call 6");
+debouncedFunction("Call 7");
+debouncedFunction("Call 8");
+//output: Debounced: Call 8
+
+//Throttling
+
+function throttle(func, interval) {
+  let lastExecutionTime = 0;
+  return function(...args) {
+    const now = Date.now();
+    if (now - lastExecutionTime >= interval) {
+      lastExecutionTime = now;
+      func(...args); 
+    }
+  };
+}
+
+function logMessage(message) {
+  console.log("Throttled: " + message);
+}
+
+const throttledFunction = throttle(logMessage, 1000);
+
+throttledFunction("Call 1"); 
+throttledFunction("Call 2"); 
+setTimeout(() => throttledFunction("Call 3"), 500); 
+setTimeout(() => throttledFunction("Call 4"), 1500);
+
+//output:
+//Throttled: Call 1
+//Throttled: Call 4
